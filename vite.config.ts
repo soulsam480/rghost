c1aabdffe916930951b54fdd3b580bb40cc7e37b
Sambit Sahoo <soulsam480@hotmail.com>
2025-11-13 20:38:42 +0530

    chore: added release

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,25 @@
-import path from 'node:path'
-import { crx } from '@crxjs/vite-plugin'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import zip from 'vite-plugin-zip-pack'
-import manifest from './manifest.config.js'
-import { name, version } from './package.json'
+import path from "node:path";
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import zip from "vite-plugin-zip-pack";
+import manifest from "./manifest.config.js";
+import { name } from "./package.json";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': `${path.resolve(__dirname, 'src')}`,
-    },
-  },
-  plugins: [
-    react(),
-    crx({ manifest }),
-    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
-  ],
-  server: {
-    cors: {
-      origin: [
-        /chrome-extension:\/\//,
-      ],
-    },
-  },
-})
+	resolve: {
+		alias: {
+			"@": `${path.resolve(__dirname, "src")}`,
+		},
+	},
+	plugins: [
+		react(),
+		crx({ manifest }),
+		zip({ outDir: "release", outFileName: `${name}.zip` }),
+	],
+	server: {
+		cors: {
+			origin: [/chrome-extension:\/\//],
+		},
+	},
+});
